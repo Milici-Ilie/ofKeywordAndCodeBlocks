1cb4410745585fe5be6697ed28a451bd32ba2929
Milici-Ilie <milicicristyan@yahoo.com>
2024-03-06 00:19:23 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+
+    //🏢🏢[KEY-WORDS]🏢🏢
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score < 5000) {
+            System.out.println("Your score was less then 5000");
+        } else {
+            System.out.println("Got here!");
+        }
+    }
 }
